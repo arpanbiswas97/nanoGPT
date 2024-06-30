89a5212918f3fe1af6c089debdffc40ab6abda26
Arpan Biswas <arpanbiswas97@gmail.com>
2024-06-30 22:47:36 +0530

    Added docllm training
    Co-authored-by: Vidya1811 <Vidya1811@users.noreply.github.com>

--- model_docllm.py
+++ model_docllm.py
@@ -6,6 +6,10 @@ from pydantic import BaseModel
 
 
 class DocLLMConfig(BaseModel):
+    vocab_size: int
+    block_size: int
+    spatial_precision: int
+    n_layer: int
     n_embd: int
     n_head: int
     bias: float
@@ -17,8 +21,8 @@ class LayerNorm(nn.Module):
 
     def __init__(self, ndim: int, bias: bool):
         super().__init__()
-        self.weight = nn.Parameter(torch.ones((2, ndim)))
-        self.bias = nn.Parameter(torch.zeros((2, ndim))) if bias else None
+        self.weight = nn.Parameter(torch.ones(ndim))
+        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
@@ -33,7 +37,7 @@ class DisentangledSpatialAttention(nn.Module):
         self.t_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
         self.s_attn = nn.Linear(config.n_embd, 2 * config.n_embd, bias=config.bias)
         # output projection
-        self.out_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
 
         self.lambda_ts = nn.Parameter(torch.ones(1))
         self.lambda_st = nn.Parameter(torch.ones(1))
@@ -46,12 +50,8 @@ class DisentangledSpatialAttention(nn.Module):
         self.n_embd = config.n_embd
         self.dropout = config.dropout
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        B, seq_len, _, n_embd = x.size()
-
-        # (B, seq_len, 2, n_embd) -> (B, seq_len, n_embd), (B, seq_len, n_embd)
-        xt, xs = torch.split(x, 1, dim=2)
-        xt, xs = xt.squeeze(), xs.squeeze()
+    def forward(self, xt: torch.Tensor, xs: torch.Tensor) -> torch.Tensor:
+        B, seq_len, n_embd = xt.size()
 
         # calculate query, key, values for all heads
         qt, kt, vt = self.t_attn(xt).split(self.n_embd, dim=2)
@@ -78,12 +78,9 @@ class DisentangledSpatialAttention(nn.Module):
 
         # (B, nh, seq_len, hs) -> (B, seq_len, n_embd)
         y = y.transpose(1, 2).contiguous().view(B, seq_len, n_embd)
-        y = self.resid_dropout(self.out_proj(y))
+        y = self.resid_dropout(self.c_proj(y))
 
-        # (B, seq_len, 2, n_embd)
-        yns = torch.cat([y.unsqueeze(2), xs.unsqueeze(2)], dim=2).contiguous()
-
-        return yns
+        return y
 
 
 class MLP(nn.Module):
@@ -95,18 +92,180 @@ class MLP(nn.Module):
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
         self.dropout = nn.Dropout(config.dropout)
 
-    def forward(self, x):
-        xt, xs = torch.split(x, 1, dim=2)
-        xt = xt.squeeze()
+    def forward(self, xt: torch.Tensor) -> torch.Tensor:
 
         xt = self.c_fc(xt)
         xt = self.gelu(xt)
         xt = self.c_proj(xt)
         xt = self.dropout(xt)
+        return xt
 
-        x = torch.cat([xt.unsqueeze(2), xs], dim=2).contiguous()
-        return x
+
+class Block(nn.Module):
+
+    def __init__(self, config: DocLLMConfig):
+        super().__init__()
+        self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
+        self.attn = DisentangledSpatialAttention(config)
+        self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
+        self.mlp = MLP(config)
+
+    def forward(self, xt: torch.Tensor, xs: torch.Tensor) -> torch.Tensor:
+        xt = xt + self.attn(self.ln_1(xt), xs)
+        xt = xt + self.mlp(self.ln_2(xt))
+        return xt
 
 
 class DocLLM(nn.Module):
-    ...
+    def __init__(self, config: DocLLMConfig):
+        super().__init()
+        assert config.vocab_size is not None
+        assert config.block_size is not None
+        self.config = config
+
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(config.vocab_size, config.n_embd),
+                spe=nn.Linear(4 * config.spatial_precision, config.n_embd),
+                drop_t=nn.Dropout(config.dropout),
+                drop_s=nn.Dropout(config.dropout),
+                h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+                ln_f=LayerNorm(config.n_embd, bias=config.bias),
+            )
+        )
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        self.transformer.wte.weight = self.lm_head.weight
+
+        # Initialise weights
+        self.apply(self._init_weights)
+        # apply special scaled init to the residual projections
+        for pn, p in self.named_parameters():
+            if pn.endswith("c_proj.weight"):
+                torch.nn.init.normal_(
+                    p, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layer)
+                )
+
+        # report number of parameters
+        print("number of parameters: %.2fM" % (self.get_num_params() / 1e6,))
+
+    def get_num_params(self, non_embedding=True):
+        """
+        Return the number of parameters in the model.
+        For non-embedding count (default), the position embeddings get subtracted.
+        The token embeddings would too, except due to the parameter sharing these
+        params are actually used as weights in the final layer, so we include them.
+        """
+        n_params = sum(p.numel() for p in self.parameters())
+        if non_embedding:
+            n_params -= self.transformer.wpe.weight.numel()
+        return n_params
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, bbox, targets=None):
+        device = idx.device
+        b, t = idx.size()
+        assert (
+            t <= self.config.block_size
+        ), f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+
+        xt = self.transformer.wte(idx)  # (b, t, n_embd)
+        xs = self.transformer.spe(bbox)  # (b, t, n_embd)
+
+        xt = self.transformer.drop_t(xt)
+        xs = self.transformer.drop_s(xs)
+
+        for block in self.transformer.h:
+            xt = block(xt, xs)
+        xt = self.transformer.ln_f(xt)
+
+        if targets is not None:
+            # If we are given some desired targets also calculate the loss
+            logits = self.lm_head(xt)
+            loss = F.cross_entropy(
+                logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
+            )
+        else:
+            # inference-time mini-optimization: only forward the lm_head on the very last position
+            logits = self.lm_head(
+                xt[:, [-1], :]
+            )  # note: using list [-1] to preserve the time dim
+            loss = None
+
+        return logits, loss
+
+    def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
+        # start with all of the candidate parameters
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        # filter out those that do not require grad
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {"params": decay_params, "weight_decay": weight_decay},
+            {"params": nodecay_params, "weight_decay": 0.0},
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(
+            f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters"
+        )
+        print(
+            f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters"
+        )
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = "fused" in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == "cuda"
+        extra_args = dict(fused=True) if use_fused else dict()
+        optimizer = torch.optim.AdamW(
+            optim_groups, lr=learning_rate, betas=betas, **extra_args
+        )
+        print(f"using fused AdamW: {use_fused}")
+
+        return optimizer
+
+    @torch.no_grad()
+    def generate(self, idx, bbox, bbox_to_fill, temperature=1.0, top_k=None):
+        """
+        idx (b, t)
+        bbox_to_fill (b, t)
+        """
+        tokens_to_generate = bbox_to_fill.size(1)
+        for i in range(tokens_to_generate):
+            # if the sequence context is growing too long we must crop it from the end
+            idx_cond = (
+                idx
+                if idx.size(1) <= self.config.block_size
+                else idx[:, -self.config.block_size :]
+            )
+            bbox_cond = (
+                bbox
+                if idx.size(1) <= self.config.block_size
+                else idx[:, -self.config.block_size :]
+            )
+
+            logits, _ = self(idx_cond, bbox_cond)
+            logits = logits[:, -1, :] / temperature
+
+            # optionally crop the logits to only the top k options
+
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+
+            # Bounding box that just got filled
+            bbox_next = bbox_to_fill[:, i]
+            idx = torch.cat((idx, idx_next), dim=1)
+            bbox = torch.cat((bbox, bbox_next), dim=1)
+
+        return idx
